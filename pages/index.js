cca89441a1c3c15226d9c8041272a5c4d670df5f
cob <34405172+xcob@users.noreply.github.com>
2023-03-14 12:22:07 -0500

    initial commit for vercel

---
+++ pages/index.js
@@ -0,0 +1,16 @@
+import styles from "./index.module.css";
+
+function HomePage() {
+    return ( 
+        <div className={styles.container}>
+            <h1 className={styles.title}>
+                Kit and Caboodle
+            </h1>
+        </div>
+        
+        )
+    
+   
+  }
+  
+  export default HomePage
